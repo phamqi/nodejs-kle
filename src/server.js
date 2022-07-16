57b02045812ab9a7dc3bca32128001ef6a1b6007
phamqi <phamquy2425@gmail.com>
2022-07-16 17:38:29 +0700

    fix 2

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import innitWebRouter from "./route/router";
 import innitApiRouter from "./route/api";
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
