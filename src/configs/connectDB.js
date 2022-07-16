90dc3dbc2b7eff5d011e2389cb21bc6e6f64fc2e
phamqi <phamquy2425@gmail.com>
2022-07-16 18:10:27 +0700

    fix 5

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,15 +1,11 @@
 import mysql from "mysql2/promise";
 require("dotenv").config();
 
-const host = process.env.LOCALHOST;
-const database = process.env.DATABASE;
-const user = process.env.USER;
-const psw = process.env.psw;
 const pool = mysql.createPool({
-  host: `${host}`,
-  user: `${user}`,
-  password: `${psw}`,
-  database: `${database}`,
+  host: "us-cdbr-east-06.cleardb.net",
+  user: "b431712a43860e",
+  password: "29e94d9e",
+  database: "heroku_7d87b189b71c1d9",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
