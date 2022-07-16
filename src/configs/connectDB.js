2a08111d129b82d987e4877b6942e887ce236369
phamqi <phamquy2425@gmail.com>
2022-07-16 16:32:24 +0700

    make it better

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -2,10 +2,14 @@ import mysql from "mysql2/promise";
 require("dotenv").config();
 
 const host = process.env.LOCALHOST;
+const database = process.env.DATABASE;
+const user = process.env.USER;
+const psw = process.env.psw;
 const pool = mysql.createPool({
   host: `${host}`,
-  user: "root",
-  database: "my_portfolio",
+  user: `${user}`,
+  password: `${psw}`,
+  database: `${database}`,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
